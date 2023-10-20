5c604d2857d9468b396817d8e5dee4132b08dbcf
Valeriya Belyayeva <848belval848@gmail.com>
2023-10-20 19:29:25 +0300

    v

--- users/authentication.py
+++ users/authentication.py
@@ -4,21 +4,31 @@ from django.utils import timezone
 
 
 class HelpdeskTokenAuthentication(TokenAuthentication):
+    def get_model(self):
+        if self.model is not None:
+            return self.model
+        from rest_framework.authtoken.models import Token
+        return Token
+
     def authenticate_credentials(self, key):
-        model = self.get_model()  # model with tokens
+        model = self.get_model()
         try:
-            token = model.objects.get(key=key)  # instance of model
+            token = model.objects.get(key=key)
         except model.DoesNotExist:
             raise exceptions.AuthenticationFailed('Token does not exist.')
 
-        if not token.user.is_authenticated:
+        user = token.user
+        if not user.is_authenticated:
             raise exceptions.AuthenticationFailed('User is not authenticated.')
-        if not token.user.is_active:
+
+        if not user.is_active:
             raise exceptions.AuthenticationFailed('User is not active.')
 
-        if not token.user.is_superuser and (timezone.now() - token.created).total_seconds() > 60:
-            token.delete()
-            raise exceptions.AuthenticationFailed('Token was deleted after 1 min of inactivity.')
-        token.created = timezone.now()
-        token.save()
-        return token.user, token
+        if not user.is_superuser:
+            if (timezone.now() - token.created).total_seconds() > 60:
+                token.delete()
+                raise exceptions.AuthenticationFailed('Token was deleted after 1 min of inactivity.')
+            token.created = timezone.now()
+            token.save()
+
+        return user, token
